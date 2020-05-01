97a6cb6203d9a5bb778931be29e03e89897a0f69
EC2 Default User <ch5cxcxxh@gmail.com>
2020-05-01 14:29:51 +0000

    fix users admins routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
-  resources :posts
-  root 'posts#index'
+  devise_for :admins, skip: :all
+  devise_scope :admin do
+    get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
+    post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
+    delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'
+  end
+  
+  devise_for :users, skip: :all
+  devise_scope :user do
+    get 'users/sign_in' => 'users/sessions#new', as: 'new_user_session'
+    post 'users/sign_in' => 'users/sessions#create', as: 'user_session'
+    delete 'users/sign_out' => 'users/sessions#destroy', as: 'destroy_user_session'
+    get 'users/sing_up' => 'users/registrations#new', as: 'new_user_registration'
+    post 'users/sign_up' => 'users/registrations#create', as: 'user_registration'
+    get 'users/password/new' => 'users/passwords#new', as: 'new_user_password'
+  end
+  
+  namespace :users do
+    resources :posts
+  end
+  root 'users/posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
